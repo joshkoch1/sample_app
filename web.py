6558c84647abc52bc6092bb6909ee79ede659bda
Josh Koch <jkoch17@gsb.columbia.edu>
2016-10-05 16:55:05 -0400

    Create About Page

--- web.py
+++ web.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # creates homepage
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('sample_page.html')
 
 # Produces IP - runs web server
 # 127.0.0.1 is the same as local host
 # http://127.0.0.1:5000/ is localhost:5000/
 # debug True allows you to change code w/out restarting server
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 app.run(debug=True)
 
 # Git commands
@@ -18,4 +24,4 @@ app.run(debug=True)
 # Staged 
 # Committed
 
-
+# {{ Will insert Python code into HTML}}
